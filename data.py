d31773fd2b0e7174c726c4a2e6162bf516bea74a
@jucesar2122 <juliodelamar@gmail.com>
2026-09-23 15:56:48 -0300

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = (https://example.com)
+urban_routes_url = 'https://example.com'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
